cd52aaa5b8d15590328339dbdfd5f116b64457d6
Ido Isaac <Idoisaac93@gmail.com>
2024-10-24 14:54:52 -0400

    Backend setup

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    env: process.env.NODE_ENV || 'development', 
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://idoisaac93:1Hardpass@cluster0.yniw8.mongodb.net/skeleton?retryWrites=true&w=majority&appName=Cluster0"||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+   (process.env.MONGO_PORT || '27017') +
+    '/portfolio' 
+    }
+    export default config
+   
+   

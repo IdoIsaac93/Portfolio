c72e28c3ddea90cbe1f52df7ffa2496902d2bad5
Ido Isaac <Idoisaac93@gmail.com>
2025-11-30 21:54:12 -0500

    Another try

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/about'
-import Contact from './components/contact'
-import Education from './components/education'
-import Project from './components/project'
-import Layout from './components/Layout'
-import Services from './components/services'
+import Home from './src/components/Home'
+import About from './src/components/about'
+import Contact from './src/components/contact'
+import Education from './src/components/education'
+import Project from './src/components/project'
+import Layout from './src/components/Layout'
+import Services from './src/components/services'
 
 const MainRouter = () => {
     return (<div>
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import MainRouter from '../MainRouter';
 
 const App = () => {
 return (
-  <Router>
+  <HashRouter>
 
     <MainRouter />
 
-  </Router>
+  </HashRouter>
 );
 };
 export default App;

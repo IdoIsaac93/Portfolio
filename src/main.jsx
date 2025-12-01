68c2b4c9ff1027a2213b02fdda2ff452b2077233
Ido Isaac <Idoisaac93@gmail.com>
2025-11-30 20:49:17 -0500

    More Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 //Main - Ido Isaac - 301374969 - 09.26.2024
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '../App.jsx'
-import '../index.css'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

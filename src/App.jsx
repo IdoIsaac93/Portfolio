cd61357268ef3535eea28bc1da39764b1943e521
Ido Isaac <Idoisaac93@gmail.com>
2025-11-30 22:16:03 -0500

    Revert some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
-import MainRouter from '../MainRouter';
+import MainRouter from './MainRouter';
 
 const App = () => {
 return (
